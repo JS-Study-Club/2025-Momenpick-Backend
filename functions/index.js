55a3324ee5975e57ddd1dd7b595bfde446fd8b89
박지영 <s2325@e-mirim.hs.kr>
2025-03-09 16:51:40 +0900

    index.js

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,12 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+// 테스트 중입니다. (배포 테스트)
 
-// const {onRequest} = require("firebase-functions/v2/https");
-// const logger = require("firebase-functions/logger");
+const functions = require("firebase-functions");
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  response.send("Hello from Firebase!");
+});
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.greet = functions.https.onRequest((request, response) => {
+  const {name} = request.body;
+  response.send(`Hello ${name}`);
+});
